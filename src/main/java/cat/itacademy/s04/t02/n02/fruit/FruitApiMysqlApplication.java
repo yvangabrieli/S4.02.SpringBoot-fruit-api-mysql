52c0e1797b8490d423cfd2bb13d6f0ec5e8f9fff
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-01-12 18:25:31 +0100

    chore(structure): rename package

--- src/main/java/cat/itacademy/s04/t02/n02/fruit/FruitApiMysqlApplication.java
+++ src/main/java/cat/itacademy/s04/t02/n02/fruit/FruitApiMysqlApplication.java
@@ -1,4 +1,4 @@
-package cat.itacademy.s04.t02.n02.fruit_api_mysql;
+package cat.itacademy.s04.t02.n02.fruit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/cat/itacademy/s04/t02/n02/fruit/FruitApiMysqlApplicationTests.java
+++ src/test/java/cat/itacademy/s04/t02/n02/fruit/FruitApiMysqlApplicationTests.java
@@ -1,4 +1,4 @@
-package cat.itacademy.s04.t02.n02.fruit_api_mysql;
+package cat.itacademy.s04.t02.n02.fruit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
