a5ec7b3ff5a9ba208d2a61338c1f1afa8a303897
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-01-12 17:53:10 +0100

    feat: Initia REST API, written in Java, using Spring Boot, that manages fruit stock data, stored in a database (H2).

---
+++ src/test/java/cat/itacademy/s04/t02/n02/fruit_api_mysql/FruitApiMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n02.fruit_api_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitApiMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
