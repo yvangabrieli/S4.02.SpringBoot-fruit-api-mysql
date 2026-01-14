c5e1e270fa1dc49dae9a71aee6eb0f73583f90f1
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-01-14 12:42:26 +0100

    feat: create provider

---
+++ src/main/java/cat/itacademy/s04/t02/n02/fruit/model/Provider.java
@@ -0,0 +1,31 @@
+package cat.itacademy.s04.t02.n02.fruit.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+@Entity
+@Table (name = "providers", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "names")
+})
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+
+
+public class Provider {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String country;
+
+
+    public Provider (String name, String country){
+        this.name = name;
+        this.country = country;
+    }
+}
