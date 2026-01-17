15536e158cd4892f4f602500f4d335747ccb5ed8
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-01-17 13:22:29 +0100

    text (fruitService) : createFruit successfully

--- src/test/java/cat/itacademy/s04/t02/n02/fruit/service/FruitServiceTest.java
+++ src/test/java/cat/itacademy/s04/t02/n02/fruit/service/FruitServiceTest.java
@@ -5,6 +5,7 @@ import cat.itacademy.s04.t02.n02.fruit.exception.BusinessRuleException;
 import cat.itacademy.s04.t02.n02.fruit.exception.ProviderNotFoundException;
 import cat.itacademy.s04.t02.n02.fruit.mapper.FruitMapper;
 import cat.itacademy.s04.t02.n02.fruit.model.Fruit;
+import cat.itacademy.s04.t02.n02.fruit.model.Provider;
 import cat.itacademy.s04.t02.n02.fruit.repository.FruitRepository;
 import cat.itacademy.s04.t02.n02.fruit.repository.ProviderRepository;
 import cat.itacademy.s04.t02.n02.fruit.validator.FruitValidator;
@@ -15,9 +16,11 @@ import org.mockito.Mock;
 import org.mockito.internal.matchers.Null;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.swing.plaf.synth.SynthPainter;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.notNull;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -55,5 +58,22 @@ public class FruitServiceTest {
 
          assertThrows(BusinessRuleException.class,
                  ()-> fruitService.createFruit(fruit));
+    }
+
+    @Test
+    void createFruit_shouldReturnFruit_whenDataIsValid(){
+        Fruit fruit = new Fruit("Apple", 5,1L);
+
+        when(fruitRepository.existsByName("Apple"))
+                .thenReturn(false);
+        when(fruitRepository.existsByProviderId(1L))
+                .thenReturn(true);
+        when(fruitRepository.save(fruit))
+                .thenReturn(fruit);
+
+        Fruit result = fruitService.createFruit(fruit); ;
+        assertEquals("Apple", result.getName());
+        assertEquals(5, result.getWeightInKilos());
+        assertNotNull(fruitRepository);
     }
 }
