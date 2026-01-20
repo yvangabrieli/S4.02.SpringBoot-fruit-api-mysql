6327d235a244af05259fa9506407ebda7d53ff2b
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-01-20 10:16:31 +0100

    text (fruitService) : test: add comprehensive unit tests for fruit create, read, update and delete operations

--- src/test/java/cat/itacademy/s04/t02/n02/fruit/service/FruitServiceTest.java
+++ src/test/java/cat/itacademy/s04/t02/n02/fruit/service/FruitServiceTest.java
@@ -14,27 +14,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.internal.matchers.Null;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import javax.swing.plaf.synth.SynthPainter;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.notNull;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class FruitServiceTest {
     @Mock
     private FruitRepository fruitRepository;
-    @Mock
-    private ProviderRepository providerRepository;
-    @Mock
-    private FruitMapper fruitMapper;
+
     @Mock
     private FruitValidator fruitValidator;
 
@@ -43,31 +37,31 @@ public class FruitServiceTest {
 
 
     @Test
-    void createFruit_shouldThrowException_whenProviderNotFound(){
+    void createFruit_shouldThrowException_whenProviderNotFound() {
         Fruit fruit = new Fruit("Apple", 5, 9L);
 
         when(fruitRepository.existsByProviderId(9L))
                 .thenReturn(false);
 
         assertThrows(ProviderNotFoundException.class,
-                () -> fruitService.createFruit(fruit) );
+                () -> fruitService.createFruit(fruit));
     }
 
     @Test
-    void createFruit_shouldThrowException_whenFruitAlreadyExist(){
-         Fruit fruit = new Fruit( "Apple", 5 , 1L);
+    void createFruit_shouldThrowException_whenFruitAlreadyExist() {
+        Fruit fruit = new Fruit("Apple", 5, 1L);
 
-         when(fruitRepository.existsByName("Apple"))
-                 .thenReturn(true);
+        when(fruitRepository.existsByName("Apple"))
+                .thenReturn(true);
 
-         Fruit result = fruit;
-         assertThrows(BusinessRuleException.class,
-                 ()-> fruitService.createFruit(result));
+        Fruit result = fruit;
+        assertThrows(BusinessRuleException.class,
+                () -> fruitService.createFruit(result));
     }
 
     @Test
-    void createFruit_shouldReturnFruit_whenDataIsValid(){
-        Fruit fruit = new Fruit("Apple", 5,1L);
+    void createFruit_shouldReturnFruit_whenDataIsValid() {
+        Fruit fruit = new Fruit("Apple", 5, 1L);
 
         when(fruitRepository.existsByName("Apple"))
                 .thenReturn(false);
@@ -76,14 +70,15 @@ public class FruitServiceTest {
         when(fruitRepository.save(fruit))
                 .thenReturn(fruit);
 
-        Fruit result = fruitService.createFruit(fruit); ;
+        Fruit result = fruitService.createFruit(fruit);
+        ;
         assertEquals("Apple", result.getName());
         assertEquals(5, result.getWeightInKilos());
         assertNotNull(fruitRepository);
     }
 
     @Test
-    void getAllFruit_shouldReturnListOfFruit(){
+    void getAllFruit_shouldReturnListOfFruit() {
         List<Fruit> fruits = List.of(
                 new Fruit("Apple", 5, 1L),
                 new Fruit("Pear", 5, 1L),
@@ -96,8 +91,8 @@ public class FruitServiceTest {
     }
 
     @Test
-    void getFruitById_shouldReturnFruitWithId(){
-               Fruit fruit = new Fruit(1L,"Apple", 5, 1L);
+    void getFruitById_shouldReturnFruitWithId() {
+        Fruit fruit = new Fruit(1L, "Apple", 5, 1L);
 
         when(fruitRepository.findById(1L))
                 .thenReturn(Optional.of(fruit));
@@ -110,49 +105,62 @@ public class FruitServiceTest {
     }
 
     @Test
-    void getFruitById_shouldThrowException_whenFruitNotFound(){
-            Fruit fruit = new Fruit("Apple",5 ,1L );
+    void getFruitById_shouldThrowException_whenFruitNotFound() {
+        Fruit fruit = new Fruit("Apple", 5, 1L);
 
-            when(fruitRepository.findById(2L)).thenReturn(Optional.empty());
+        when(fruitRepository.findById(2L)).thenReturn(Optional.empty());
 
-            Fruit result = fruit;
+        Fruit result = fruit;
 
-            assertThrows(FruitNotFoundException.class, () -> fruitService.getFruitById(2L));
+        assertThrows(FruitNotFoundException.class, () -> fruitService.getFruitById(2L));
     }
 
     @Test
-    void updateFruit_shouldSuccess_whenFruitIsValid(){
-    Fruit existing = new Fruit(1L, "Apple", 5, 1L);
-    Fruit newData = new Fruit(1L, "Green Apple", 3, 1L);
-
-    doNothing().when(fruitValidator).validate(any(Fruit.class));
-    when(fruitRepository.findById(1L))
-            .thenReturn(Optional.of(existing));
-    when(fruitRepository.save(existing))
-            .thenReturn(newData);
-
-
-    Fruit result = fruitService.updateFruit(1L, newData);
-
-    assertEquals(1L, result.getId());
-    assertEquals("Green Apple", result.getName());
-    assertEquals(3, result.getWeightInKilos());
-    assertEquals(1L, result.getProviderId());
-    }
-
-    @Test
-    void updateFruit_shouldThrowException_whenFruitNotFound(){
-        Fruit newData = new Fruit("Apple", 4 , 1L);
+    void updateFruit_shouldSuccess_whenFruitIsValid() {
+        Fruit existing = new Fruit(1L, "Apple", 5, 1L);
+        Fruit newData = new Fruit(1L, "Green Apple", 3, 1L);
 
         doNothing().when(fruitValidator).validate(any(Fruit.class));
+        when(fruitRepository.findById(1L))
+                .thenReturn(Optional.of(existing));
+        when(fruitRepository.save(existing))
+                .thenReturn(newData);
+
+
+        Fruit result = fruitService.updateFruit(1L, newData);
+
+        assertEquals(1L, result.getId());
+        assertEquals("Green Apple", result.getName());
+        assertEquals(3, result.getWeightInKilos());
+        assertEquals(1L, result.getProviderId());
+    }
+
+    @Test
+    void updateFruit_shouldThrowException_whenFruitNotFound() {
+        Fruit newData = new Fruit("Apple", 4, 1L);
 
         when(fruitRepository.findById(1L))
                 .thenReturn(Optional.empty());
-        assertThrows(FruitNotFoundException.class, ()-> fruitService.updateFruit(1L, newData));
+
+        assertThrows(FruitNotFoundException.class, () -> fruitService.updateFruit(1L, newData));
     }
 
     @Test
-    void deleteFruit_shouldSuccess_whenFruitFound(){
+    void deleteFruit_shouldSuccess_whenFruitFound() {
+
+        when(fruitRepository.existsById(1L))
+                .thenReturn(true);
+        fruitService.deleteFruit(1L);
+        verify(fruitRepository, times(1)).deleteById(1L);
+    }
+
+    @Test
+    void deleteFruit_shouldThrowException_whenFruitNotFound() {
+
+        when(fruitRepository.existsById(2L))
+                .thenReturn(false);
+
+        assertThrows(FruitNotFoundException.class, () -> fruitService.deleteFruit(2L));
 
     }
 }
